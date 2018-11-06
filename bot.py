607748b1c35a6f3d9ee39bac62d1c52b674ef9c9
zaooozi57@gmail.com <zaooozi57@gmail.com>
2018-11-06 17:47:39 +0300

    20 commit

--- bot.py
+++ bot.py
@@ -8,23 +8,23 @@ bot = telebot.TeleBot(config.token, threaded=False)
 
 @bot.message_handler(content_types="text")
 
-def test_handler(message):
+def text_handler(message):
 
-    if(message.text()=='/start'):
+#   if(message.text()=='/start'):
     
-        answer = 'Здравствуйте, этот бот представляет Церковь Христианская Миссия'
+#       answer = 'Здравствуйте, этот бот представляет Церковь Христианская Миссия'
         
-    elif(message.text()=='/help'):
+#    elif(message.text()=='/help'):
     
-        answer = '''На данный момент поддерживаются 5 команд,
-            /contacts Предоставляет информацию о наших контактах
-            /links Полезные ссылки
-            /hotline Горячая линия телеграме с нашими служителями
-            /informations О нас
-            /legal Юридический уголок'''
-    else:
+#        answer = '''На данный момент поддерживаются 5 команд,
+#            /contacts Предоставляет информацию о наших контактах
+#            /links Полезные ссылки
+#            /hotline Горячая линия телеграме с нашими служителями
+#            /informations О нас
+#            /legal Юридический уголок'''
+#   else: 
         
-        answer = message.text()
+    answer = message.text()
     
     bot.send_message(message.chat.id, answer)
 
