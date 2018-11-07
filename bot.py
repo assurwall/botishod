ba6aa990e79f20bb8fc37a0f628a3435a2123f11
zaooozi57@gmail.com <zaooozi57@gmail.com>
2018-11-07 15:16:05 +0300

    auto commit

--- bot.py
+++ bot.py
@@ -8,6 +8,35 @@ from telebot import types
 
 bot = telebot.TeleBot(config.token, threaded=False)
 
+def send_main_menu(message):
+    
+    hotline_button=types.InlineKeyboardButton(text='Горячая линия', callback_data='hotline_query')
+        
+    informations_button=types.InlineKeyboardButton(text='О нас', callback_data='informations_query')
+        
+    contacts_button=types.InlineKeyboardButton(text='Контакты', callback_data='contacts_query')
+        
+    links_button=types.InlineKeyboardButton(text='Полезные ссылки', callback_data='links_query')
+        
+    legal_button=types.InlineKeyboardButton(text='Юридический уголок', callback_data='legal_query')
+        
+        
+    keyboard=types.InlineKeyboardMarkup()
+        
+        
+    keyboard.add(hotline_button)
+        
+    keyboard.add(informations_button)
+        
+    keyboard.add(contacts_button)
+        
+    keyboard.add(links_button)
+        
+    keyboard.add(legal_button)
+        
+        
+    bot.send_message(message.chat.id, 'Выберите интересующий пункт из меню.', reply_markup = keyboard)
+
 
 @bot.message_handler(content_types="text")
 
@@ -19,41 +48,13 @@ def text_handler(message):
         
         bot.send_message(message.chat.id, answer)
     
-
     elif(message.text=='/help'):
+        
+        send_main_menu(message)
+        
+#    else:
     
-        hotline_button=types.InlineKeyboardButton(text='Горячая линия', callback_data='hotline_query')
-        
-        informations_button=types.InlineKeyboardButton(text='О нас', callback_data='informations_query')
-        
-        contacts_button=types.InlineKeyboardButton(text='Контакты', callback_data='contacts_query')
-        
-        links_button=types.InlineKeyboardButton(text='Полезные ссылки', callback_data='links_query')
-        
-        legal_button=types.InlineKeyboardButton(text='Юридический уголок', callback_data='legal_query')
-        
-        
-        keyboard=types.InlineKeyboardMarkup()
-        
-        
-        keyboard.add(hotline_button)
-        
-        keyboard.add(informations_button)
-        
-        keyboard.add(contacts_button)
-        
-        keyboard.add(links_button)
-        
-        keyboard.add(legal_button)
-        
-        
-        bot.send_message(message.chat.id, 'Выберите интересующий пункт из меню.', reply_markup = keyboard)
-        
-
-        
-    else:
-    
-        bot.send_message(message.chat.id, 'Я вас не понимаю.')
+#        bot.send_message(message.chat.id, 'Я вас не понимаю.')
         
 @bot.callback_query_handler(func=lambda inline_query: True)
 
@@ -68,7 +69,8 @@ def inline_handler(inline_query):
                 message_id=inline_query.message.message_id,
     
                 text='''Вы можете позвонить по телефону
-                                    нашей горячей линии''',
+                        нашей горячей линии 8-800-333-09-81,
+                        А также связаться с нами по ''',
                                     
                 parse_mode='Markdown')
     
