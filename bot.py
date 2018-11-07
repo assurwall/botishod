9941be43d3d80134f214add83d6271067f7a8f50
zaooozi57@gmail.com <zaooozi57@gmail.com>
2018-11-07 16:25:08 +0300

    auto commit

--- bot.py
+++ bot.py
@@ -62,15 +62,15 @@ def inline_handler(inline_query):
 
     if(inline_query.data=='hotline_query'):
         
-#        keyboard=types.InlineKeyboardMarkup()
+        keyboard=types.InlineKeyboardMarkup()
         
-#        chat_button=types.InlineKeyboardButton(text='Чат', url='https://t.me/joinchat/HUGe2kdgu8_3lkWy2qvrvA')
+        chat_button=types.InlineKeyboardButton(text='Чат', url='https://t.me/joinchat/HUGe2kdgu8_3lkWy2qvrvA')
         
-#        back_button=types.InlineKeyboardButton(text='Назад', call_backdate='main_menu_query')
+        back_button=types.InlineKeyboardButton(text='Назад', call_backdate='main_menu_query')
     
-#        keyboard.add(chat_button)
+        keyboard.add(chat_button)
         
-#        keyboard.add(back_button)
+        keyboard.add(back_button)
     
         bot.edit_message_text(
             
@@ -79,14 +79,14 @@ def inline_handler(inline_query):
                 message_id=inline_query.message.message_id,
     
                 text='Вы можете позвонить нам по бесплатному номеру '
-                ' 8-800-333-09-81,'
-                ' Пишите нам:'
-                ' @Yarik78'
-                ' @Zaosi'
-                ' @Pomoth'
-                ' Наш чат, если бан:',
+                '8-800-333-09-81, '
+                'Пишите нам: '
+                '@Yarik78 '
+                '@Zaosi '
+                '@Pomoth '
+                'Наш чат, если бан: ',
                 
-#                reply_markup=keyboard,
+                reply_markup=keyboard,
                                     
                 parse_mode='Markdown')
     
