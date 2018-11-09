b45e48db2706c952752c552dc3b729d9618221f7
zaooozi57@gmail.com <zaooozi57@gmail.com>
2018-11-09 12:07:04 +0300

    auto commit

--- config.py
+++ config.py
@@ -2,4 +2,8 @@
 
 # Бот с именем PomothBot
 
-token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
+#token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
+
+# Бот с именeм @test1819_bot
+
+token = '775475818:AAGN7d2KFdFOSgA-ZNydu8-9W9BiSXSs3lw'
